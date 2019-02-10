acb4dce182b1a49471eac7fdc0da30fc3649b04a
edbaeza <baeza.eric37@gmail.com>
2019-02-10 03:05:19 -0500

    made changes to route to signup and recovery components (pages)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignupComponent } from './components/signup/signup.component';
+import { RecoveryComponent } from './components/recovery/recovery.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'signup', component: SignupComponent},
+  {path: 'recovery', component: RecoveryComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { RecoveryComponent } from './components/recovery/recovery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    RecoveryComponent
   ],
   imports: [
     BrowserModule,
