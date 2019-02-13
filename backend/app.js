b95d70b902d307db445c0b79dc44a663fa9ef935
Tung Dao <dvtung98@gmail.com>
2019-02-13 10:09:13 -0500

    add Cross-Origin Resource Sharing, fix environment variables

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,12 @@
 */
 
 const express = require("express");
-const userRoutes = require("./routes/UserRoutes");
 const app = express();
 const path = require("path");
 const bodyParser = require("body-parser");
+const CORS = require("./middlewares/CORS");
+
+const userRoutes = require("./routes/UserRoutes");
 
 //use body parser to read POST HTTP request and JSON params
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,6 +16,9 @@ app.use(bodyParser.json());
 
 //Help backend run the static files of frontend in the same port (3000). NOT RECOMMEND for developement use
 //app.use(express.static(path.join(__dirname, "dist")));
+
+//For Cross-Origin Resource Sharing between 2 different ports
+app.use(CORS.connect);
 
 //all route /api/user/... will go to userRoutes
 app.use("/api/user", userRoutes);
--- src/app/services/user-auth.service.ts
+++ src/app/services/user-auth.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { environment } from "src/environments/environment.prod";
+import { environment } from "src/environments/environment";
 import { UserModel } from "src/app/models/UserAuth.model";
 
 //backend api url for communication (Port 3000)
 // const BACKEND_URL = environment.apiUrl + "/user/";
-const BACKEND_URL = environment.apiUrl + "/user/";
+const BACKEND_URL = environment.apiUrl + "/user";
 
 @Injectable({
   providedIn: "root"
