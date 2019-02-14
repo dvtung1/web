f40fe4c6629553eb2ee6d955a6ad6c23eacce308
Tung Dao <dvtung98@gmail.com>
2019-02-14 18:18:11 -0500

    Fix authentication middleware, add log out feature in backend, add home page

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -5,7 +5,7 @@
 const express = require("express");
 const router = express.Router();
 const UserController = require("../controllers/UserController");
-
+const userAuthentication = require("../middlewares/userAuthentication.middleware");
 //Input route, http verb, along with controllers below
 
 //route /signup will execute createAccount method
@@ -13,4 +13,6 @@ router.post("/signup", UserController.createAccount);
 router.post("/login", UserController.signIn);
 router.post("/recovery", UserController.recoveryPassword);
 router.post("/resend", UserController.resendConfirmation);
+router.get("/logout", UserController.logOut);
+
 module.exports = router;
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { Routes, RouterModule } from "@angular/router";
 import { SignupComponent } from "./components/signup/signup.component";
 import { RecoveryComponent } from "./components/recovery/recovery.component";
 import { LoginComponent } from "./components/login/login.component";
+import { HomeComponent } from "./components/home/home.component";
 
 const routes: Routes = [
-  { path: "", redirectTo: "/login", pathMatch: "full" },
+  { path: "", component: HomeComponent },
   { path: "login", component: LoginComponent },
   { path: "signup", component: SignupComponent },
   { path: "recovery", component: RecoveryComponent }
