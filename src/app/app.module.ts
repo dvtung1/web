ab67e78aa742f241300833d93620909f858247fa
Arjun <sriniv58@purdue.edu>
2019-02-16 14:58:41 -0500

    Added basic navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { RecoveryComponent } from "./components/recovery/recovery.component";
 import { UserAuthService } from "./services/user-auth.service";
 import { LoginComponent } from "./components/login/login.component";
 import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { HomeComponent } from './components/home/home.component';
     SignupComponent,
     RecoveryComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [UserAuthService],
