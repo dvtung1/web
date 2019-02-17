c1d03502b23b0c6ed2acb072c28c38292f9c3c92
edbaeza <baeza.eric37@gmail.com>
2019-02-17 15:20:52 -0500

    added a component to manage account info

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { SignupComponent } from "./components/user-auth/signup/signup.component"
 import { RecoveryComponent } from "./components/user-auth/recovery/recovery.component";
 import { LoginComponent } from "./components/user-auth/login/login.component";
 import { HomeComponent } from "./components/main/home/home.component";
+import { ManageComponent } from './components/user-auth/manage/manage.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "login", component: LoginComponent },
   { path: "signup", component: SignupComponent },
-  { path: "recovery", component: RecoveryComponent }
+  { path: "recovery", component: RecoveryComponent },
+  { path: "manage", component: ManageComponent }
 ];
 
 @NgModule({
