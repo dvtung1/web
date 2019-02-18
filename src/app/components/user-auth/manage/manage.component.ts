95b42cd9b00706ce3222a8535f3c3cfc930aee73
edbaeza <baeza.eric37@gmail.com>
2019-02-17 20:20:40 -0500

    made a basic page for managing account info

--- src/app/components/user-auth/manage/manage.component.ts
+++ src/app/components/user-auth/manage/manage.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage.component.css']
 })
 export class ManageComponent implements OnInit {
-
+  editEmail: boolean = false; // if use pused button for editing email
+  editPassword: boolean = false; // if user pushed button for editing password
+  
   constructor() { }
 
   ngOnInit() {
   }
 
+  editEmailToggle() {
+    this.editEmail = !this.editEmail;
+  }
+  editPasswordToggle() {
+    this.editPassword = !this.editPassword;
+  }
 }
