bb01c51b925c1a0cc45dfe319db40a0aa6a3dc40
dvtung1 <dvtung98@gmail.com>
2019-02-20 22:16:01 -0500

    Update class object using latest es6

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,7 +1,39 @@
-function Model() {
-  this.email = "";
-  this.password = "";
-  this.username = "";
+class User {
+  constructor() {
+    this._email = "";
+    this._password = "";
+    this._username = "";
+  }
+  set email(email) {
+    if (typeof email === "string") {
+      this._email = email;
+    } else {
+      return "invalid input";
+    }
+  }
+  get email() {
+    return this._email;
+  }
+  set password(password) {
+    if (typeof password === "string") {
+      this._password = password;
+    } else {
+      return "invalid input";
+    }
+  }
+  get password() {
+    return this._password;
+  }
+  set username(username) {
+    if (typeof username === "string") {
+      this._username = username;
+    } else {
+      return "invalid input";
+    }
+  }
+  get username() {
+    return this._username;
+  }
 }
 
-module.exports = Model;
+module.exports = User;
