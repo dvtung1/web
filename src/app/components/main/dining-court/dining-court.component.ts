1ede9e102a00495eadeca17df8276da8619c46d6
Arjun <sriniv58@purdue.edu>
2019-03-15 22:37:32 +0000

    Added code to check for swear words

--- src/app/components/main/dining-court/dining-court.component.ts
+++ src/app/components/main/dining-court/dining-court.component.ts
@@ -72,28 +72,64 @@ export class DiningCourtComponent implements OnInit, OnDestroy {
   }
   postComment(form: NgForm) {
     var inputComment = form.value.comment;
-    var swearWords = 
-    ["anal", "anus", "arsehole", "ass", "asshole",
-    "bastard", "bastardo", "bbc", "bdsm", "bitch", "bitches", "blowjob", "bondage", "boner", "boob", "boobs", "booty", "bullshit", "butt", "buttcheeks", "butthole",
-    "camel toe", "cameltoe", "clit", "clitoris", "clusterfuck", "cock", "cocks", "creampie", "cum", "cunt",
-    "deepthroat", "dick", "dildo", "doggystyle", "doggy style", 
-    "ejaculate", "ejaculation", "erotic", "erotism",
-    "faggot", "fingering", "fisting", "fuck", "fucker", "fucking", "fucked", "fucktard",
-    "gangbang", "genital", "genitalia", "genitals",
-    "handjob", "hand job", "hentai", "homoerotic", "hump", "humped", "humping",
-    "incest", "intercourse",
-    "jack off", "jerk off", "jizz",
-    "kink", "kinky",
-    "masturbate", "milf", "motherfucker",
-    "negro", "nigger", "nigga", "nipple", "nipples",
-    "orgasm", "orgasmic", "orgy",
-    "penis", "porn", "porno", "pornography", "pube", "pubes", "pubic", "pussy",
-    "queef", "rape", "raped", "raping",
-    "scat", "scrotum", "semen", "sex", "sexy", "sext", "sexting", "shit", "shitty", "shat", "slut",
-    "threesome", "tit", "tits", "titties", "titty",
-    "vagina", "vibrator",
-    "wank"];
     this.diningService.postComment(inputComment, this.diningNameBackend);
+    // var swearWords = 
+    // ["anal", "anus", "arsehole", "ass", "asshole",
+    // "bastard", "bastardo", "bbc", "bdsm", "bitch", "bitches", "blowjob", "bondage", "boner", "boob", "boobs", "booty", "bullshit", "butt", "buttcheeks", "butthole",
+    // "camel toe", "cameltoe", "clit", "clitoris", "clusterfuck", "cock", "cocks", "creampie", "cum", "cunt",
+    // "deepthroat", "dick", "dildo", "doggystyle", "doggy style", 
+    // "ejaculate", "ejaculation", "erotic", "erotism",
+    // "faggot", "fingering", "fisting", "fuck", "fucker", "fucking", "fucked", "fucktard",
+    // "gangbang", "genital", "genitalia", "genitals",
+    // "handjob", "hand job", "hentai", "homoerotic", "hump", "humped", "humping",
+    // "incest", "intercourse",
+    // "jack off", "jerk off", "jizz",
+    // "kink", "kinky",
+    // "masturbate", "milf", "motherfucker",
+    // "negro", "nigger", "nigga", "nipple", "nipples",
+    // "orgasm", "orgasmic", "orgy",
+    // "penis", "porn", "porno", "pornography", "pube", "pubes", "pubic", "pussy",
+    // "queef", "rape", "raped", "raping",
+    // "scat", "scrotum", "semen", "sex", "sexy", "sext", "sexting", "shit", "shitty", "shat", "slut",
+    // "threesome", "tit", "tits", "titties", "titty",
+    // "vagina", "vibrator",
+    // "wank"];
+    // if (swearWords.some(function(v) {
+    //   return inputComment.indexOf(v) >= 0;
+    // })) {
+    //   this.diningService.postComment(inputComment, this.diningNameBackend);
+    // }
+    // else {
+    //   switch (this.diningName) {
+    //     case "1bowl":
+    //     window.location.assign("/1bowl");
+    //     window.alert("Your comment contains inappropriate language and will not be posted. Please post a new comment.");
+    //     break;
+    //     case "earhart":
+    //     window.location.assign("/earhart");
+    //     window.alert("Your comment contains inappropriate language and will not be posted. Please post a new comment.");
+    //     break;
+    //     case "ford":
+    //     window.location.assign("/ford");
+    //     window.alert("Your comment contains inappropriate language and will not be posted. Please post a new comment.");
+    //     break;
+    //     case "hillenbrand":
+    //     window.location.assign("/hillenbrand");
+    //     window.alert("Your comment contains inappropriate language and will not be posted. Please post a new comment.");
+    //     break;
+    //     case "wiley":
+    //     window.location.assign("/wiley");
+    //     window.alert("Your comment contains inappropriate language and will not be posted. Please post a new comment.");
+    //     break;
+    //     case "windsor":
+    //     window.location.assign("/windsor");
+    //     window.alert("Your comment contains inappropriate language and will not be posted. Please post a new comment.");
+    //     break;
+    //     // case "pete's za":
+    //     // window.location.assign("/1bowl");
+    //     // break;
+    //   }
+    // }
   }
   deleteComment(commentId: string) {
     this.diningService.removeComment(commentId);
