cbf6b55d67c25910b18494070bfa6c05ce9b4c3f
Arjun <sriniv58@purdue.edu>
2019-03-31 16:09:19 -0400

    Button with link to feedback form component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from "./components/main/home/home.component";
 import { ManageComponent } from "./components/user-auth/manage/manage.component";
 import { DiningCourtComponent } from "./components/main/dining-court/dining-court.component";
 import { CreateCommentComponent } from "./components/main/create-comment/create-comment.component";
+import { FeedbackFormComponent } from "./components/user-auth/feedback-form/feedback-form.component";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
@@ -16,7 +17,8 @@ const routes: Routes = [
   { path: "manage", component: ManageComponent },
   { path: "dining/:diningName", component: DiningCourtComponent },
   { path: "dining/:diningName/create/:id", component: CreateCommentComponent },
-  { path: "dining/:diningName/create", component: CreateCommentComponent }
+  { path: "dining/:diningName/create", component: CreateCommentComponent },
+  { path: "feedbackform", component: FeedbackFormComponent}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { ManageComponent } from "./components/user-auth/manage/manage.component"
 import { MenuComponent } from "./components/main/menu/menu.component";
 import { DiningCourtComponent } from "./components/main/dining-court/dining-court.component";
 import { CreateCommentComponent } from './components/main/create-comment/create-comment.component';
-import { FeedbackFormComponent } from './components/main/feedback-form/feedback-form.component';
+import { FeedbackFormComponent } from './components/user-auth/feedback-form/feedback-form.component';
 
 @NgModule({
   declarations: [
