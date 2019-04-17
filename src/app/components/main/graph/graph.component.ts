2a2236e17aaee44a480e9aba4493d4882c84d1f8
Eric Baeza <baeza.eric37@gmail.com>
2019-04-17 19:43:06 -0400

    get the average ratings from the backend.

--- src/app/components/main/graph/graph.component.ts
+++ src/app/components/main/graph/graph.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 import { ChartOptions, ChartType, ChartDataSets } from "chart.js";
 import * as pluginDataLabels from "chartjs-plugin-datalabels";
 import { Label } from "ng2-charts";
